01905843b581f19d7252927652aa7db56ee6b97e
iehtian <1850453937@qq.com>
2025-11-16 17:07:47 +0800

    将打印结果传递出去

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -4,16 +4,25 @@
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue'
+  import { ref, watch } from 'vue'
   const input = ref('')
   import apiClient from '@/api/axios';
+  const Search_res = ref<unknown[][]>([])
   const submitUserForm = () => {
     return apiClient.get('painters')
       .then((response) => {
-        console.log(response.data);
+        Search_res.value = response.data
+        console.log(Search_res.value);
       })
       .catch((error) => {
         console.error('There was an error!', error);
-      });
+      })
   }
+  const emit = defineEmits<{
+    (e: 'update:search-results', value: unknown[][]): void
+  }>()
+  watch(Search_res, (newVal) => {
+    console.log('Search results updated:', newVal);
+    emit('update:search-results', newVal)
+  })
 </script>
